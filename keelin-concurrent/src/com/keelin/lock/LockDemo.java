17f42603db6996ceb349644205593641bb2bb877
sunkening <812610493@qq.com>
2020-11-11 23:18:20 +0800

    Lock

---
+++ keelin-concurrent/src/com/keelin/lock/LockDemo.java
@@ -0,0 +1,25 @@
+package keelin.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 使用显式锁的范式
+ */
+public class LockDemo {
+    private Lock lock = new ReentrantLock();
+    private int count;
+
+    public void increment(){
+        lock.lock();
+        try{
+            count++;
+        }finally{
+            lock.unlock();//一定要在finally中释放锁
+        }
+    }
+
+    public synchronized void increment2(){
+        count++;
+    }
+}
